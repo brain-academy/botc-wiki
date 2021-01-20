cdcfeacf70308785334b576c797905c7d42b56e2
David Urvoy <daviid.urvoy@gmail.com>
2021-01-20 12:12:27 +0000

    style: simplify website tab title

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome`}
+      description="Brain Academy website homepage">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
