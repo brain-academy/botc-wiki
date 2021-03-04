e85b05ed7b545ea7c7831127e2de342b6d255070
David Urvoy <daviid.urvoy@gmail.com>
2021-03-04 17:34:01 +0000

    doc: remove  links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,15 +84,9 @@ module.exports = {
             {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/brain-academy/wiki/tree/master/',
                 },
                 blog: {
                     showReadingTime: true,
-                    // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/brain-academy/wiki/tree/master/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
