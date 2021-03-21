17afe09042351cf97226bd5df02be415ba35a983
David Urvoy <daviid.urvoy@gmail.com>
2021-03-21 09:37:27 +0000

    feat: Add modules cheatsheets and night orders

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,7 @@ module.exports = {
     someSidebar: {
         Accueil: [ 'index' ],
         Modules: fs.readdirSync('docs/modules/').map(module_file => `modules/${module_file.split('.')[ 0 ]}`),
+        Fiches: fs.readdirSync('docs/fiches/').map(cheatsheet_file => `fiches/${cheatsheet_file.split('.')[ 0 ]}`),
         Roles: fs.readdirSync('docs/roles/').map(role_file => `roles/${role_file.split('.')[ 0 ]}`)
     },
 };
