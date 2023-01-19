12a51d5f72b64307a5c4b7cdd32ee03731e6faee
Le-Pacha <dubief.alexandre@free.fr>
2023-01-19 13:19:47 +0000

    INIT pages guide

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,7 @@ module.exports = {
         Accueil: [ 'index' ],
         Modules: MODULES.map(module => `modules/${module}`),
         Roles: fs.readdirSync('docs/roles/').map(role_file => `roles/${role_file.split('.')[ 0 ]}`),
-        Documents: ['glossaire', 'deroulement', 'regles', 'strategies', 'conteurs'],
+        Documents: ['glossaire', 'deroulement', 'regles', 'strategies', 'conteurs', 'guide_athee', 'guide_folie', 'guide_legion', 'guide_marionnette'],
         Fiches: FICHES.map(fiche => `fiches/${fiche}`),
     },
 };
