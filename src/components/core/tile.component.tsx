b9f892f576cab64b65b559bf92f80c444fb2bb46
Le-Pacha <99563806+Le-Pacha@users.noreply.github.com>
2026-08-28 14:38:26 +0000

    [FIX] CSS issues

--- src/components/core/tile.component.tsx
+++ src/components/core/tile.component.tsx
@@ -16,12 +16,12 @@ export const Tile = ({props: {path, iconPath, name, theme: {color, darkBackgroun
 	const {colorMode} = useColorMode()
 	return <a href={path}
 		css={{
-			display: 'flex', flexDirection: 'column',
-			border: `5px double ${color}`, width: '160px', height: '160px', textAlign: 'center',
+			display: 'flex', flexDirection: 'column', position: 'relative',
+			boxSizing: 'border-box', border: `5px double ${color}`, width: '160px', height: '160px', textAlign: 'center',
 			'&:hover': {backgroundColor: colorMode === 'dark' ? darkBackgroundColor : lightBackgroundColor, textDecoration: 'none'}
 		}}
 	>
-		<img src={iconPath} height='140px' />
-		<span css={{fontWeight: 'bold', position: 'relative', top: '-30px', color}}>{name}</span>
+		<img src={iconPath} height='130px' css={{flexShrink: 0, objectFit: 'contain'}} />
+		<span css={{fontWeight: 'bold', lineHeight: '1.1', position: 'absolute', bottom: '6px', left: '4px', right: '4px', padding: '0 2px 2px', color}}>{name}</span>
 	</a>
 }
