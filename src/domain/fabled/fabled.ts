b22a88571853559716dc306c443f2602543c0d49
Le-Pacha <99563806+Le-Pacha@users.noreply.github.com>
2026-08-28 15:00:44 +0000

    [FIX] Fabled names

--- src/domain/fabled/fabled.ts
+++ src/domain/fabled/fabled.ts
@@ -14,17 +14,17 @@ export default class Fabled implements BotcEntity {
 
 export const FABLED = {
 	ANGE: Fabled.new({name: `Ange`, originalName: 'Angel'}),
-	ARTISAN: Fabled.new({name: `Artisan`, originalName: 'Toymaker'}),
-	BARATINEUR: Fabled.new({name: `Baratineur`, originalName: 'Fibbin'}),
-	BIBLIOTHECAIRE: Fabled.new({name: `Bibliothécaire`, filename: 'bibliothecaire', originalName: 'Hells Librarian'}),
+	BIBELOTIER: Fabled.new({name: `Bibelotier`, originalName: 'Toymaker'}),
+	BIBLIOTHECAIRE: Fabled.new({name: `Bibliothécaire de l'enfer`, filename: 'bibliothecaire', originalName: 'Hells Librarian'}),
 	BOUDDHISTE: Fabled.new({name: `Bouddhiste`, originalName: 'Buddhist'}),
 	DEUS_EX_FIASCO: Fabled.new({name: `Deus ex Fiasco`, filename: 'deus-ex-fiasco', originalName: 'Deus ex Fiasco'}),
 	DJINN: Fabled.new({name: `Djinn`, filename: 'djinn', originalName: 'Djinn'}),
 	DUCHESSE: Fabled.new({name: `Duchesse`, originalName: 'Duchess'}),
 	PASSEUR: Fabled.new({name: `Passeur`, originalName: 'Ferryman'}),
-	PRECHEUR_DE_L_APOCALYPSE: Fabled.new({name: `Precheur de l'apocalypse`, filename: 'precheur-de-l-apocalypse', originalName: 'Doomsayer'}),
-	PROTECTRICE: Fabled.new({name: `Protectrice`, originalName: 'Spirit of Ivory'}),
+	PRECHEUR_DE_L_APOCALYPSE: Fabled.new({name: `Prêcheur de l'apocalypse (Messager de malheur)`, filename: 'precheur-de-l-apocalypse', originalName: 'Doomsayer'}),
 	REVOLUTIONNAIRE: Fabled.new({name: `Révolutionnaire`, filename: 'revolutionnaire', originalName: 'Revolutionnary'}),
 	SENTINELLE: Fabled.new({name: `Sentinelle`, originalName: 'Sentinel'}),
-	TROUBLE_FETE: Fabled.new({name: `Trouble-fête`, filename: 'trouble-fete', originalName: 'Fiddler'}),
+	TAQUIN: Fabled.new({name: `Taquin`, originalName: 'Fibbin'}),
+	TOUR_D_IVOIRE: Fabled.new({name: `Tour d'Ivoire (Esprit d'Ivoire)`, filename: 'tour-d-ivoire', originalName: 'Spirit of Ivory'}),
+	TROUBLE_FETE: Fabled.new({name: `Trouble-fête (Violoniste)`, filename: 'trouble-fete', originalName: 'Fiddler'}),
 }
